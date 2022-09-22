f475fecdb988797fcea76f0530efa031fd924a7a
Irena <irina0773555666@gmail.com>
2022-09-22 20:22:38 +0900

    データベースの作成

--- frontend/src/pages/Top.tsx
+++ frontend/src/pages/Top.tsx
@@ -4,18 +4,25 @@ import appImage from "../images/app-logo.png"
 import redBar from "../images/red-bar.png"
 
 export const Top: React.FC<{ setAccessToken: (accessToken: string | null) => any }> = ( {setAccessToken} ) => {
-    return (
+    
+  const buttonAlert = () =>{
+    console.log("hoge")
+  }
+  return (
         <div className="Top">
         <img
           alt=""
           className="App-logo"
           src={appImage}
         />
+        
         <div className="Tab">
           <div className="Tab-red"><p className="Tab-red-text">全て</p></div>
           <div className="Tab-white"><p className="Tab-white-text">査定中</p></div>
           <div className="Tab-white"><p className="Tab-white-text">呼び出し中</p></div>
           <div className="Tab-white"><p className="Tab-white-text">対応完了</p></div>
+          <button type="button" className="Tab-white"onClick={buttonAlert}><p className="Tab-white-text">番号発行</p></button>
+          
         </div>
         <div className="Table">
           <p className="Table-reserve-num">受付番号</p>
