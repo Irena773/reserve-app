34e7888968a716b820cd9001db1babc9e5f48b90
Irena773 <ozawa.nanami.18@shizuoka.ac.jp>
2022-10-03 01:48:04 +0900

    DB上のデータを表示

--- frontend/src/pages/Top.tsx
+++ frontend/src/pages/Top.tsx
@@ -1,31 +1,32 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import {useState} from "react";
 import "./Top.css"
 import axios from "../api/axios";
 import appImage from "../images/app-logo.png"
 import redBar from "../images/red-bar.png"
 
-type SecondHand = {
-  id:number,
-  reserve_time:string,
-  elapsed_time:string,
-  status:string,
-}
-
 export const Top: React.FC<{ setAccessToken: (accessToken: string | null) => any }> = ( {setAccessToken} ) => {
 
-  const [data, setData] = React.useState();
+  const [data, setData] = useState([]);
   const url = "http://127.0.0.1:8000/secondhand";
 
-  const GetData = async() => {
-    try{
-      const response = axios.get(url);
-      console.log("できた")
-      console.log(response);
-    }catch(error){
-      console.error(error);
-    }
-  };
+ type SecondHand = {
+  status: string,
+  id: number,
+  reserve_time : number,
+  elapsed_time : number,
+  done: boolean
+ }
+  const getData = async () => {
+    await axios.get(url)
+    .then((res) => {
+      setData(res.data);
+    })
+  }
+
+  useEffect(()=>{
+    getData();
+  }, []);
 
   return (
         <div className="Top">
@@ -34,13 +35,12 @@ export const Top: React.FC<{ setAccessToken: (accessToken: string | null) => any
           className="App-logo"
           src={appImage}
         />
-        
         <div className="Tab">
           <button className="Tab-white"><p className="Tab-white-text">全て</p></button>
           <button className="Tab-white"><p className="Tab-white-text">査定中</p></button>
           <button className="Tab-white"><p className="Tab-white-text">呼び出し中</p></button>
           <button className="Tab-white"><p className="Tab-white-text">対応完了</p></button>
-          <button type="button" className="Tab-red" onClick={GetData}><p className="Tab-red-text">番号発行</p></button>
+          <button type="button" className="Tab-red" onClick={getData} ><p className="Tab-red-text">番号発行</p></button>
           
         </div>
         <div className="Table">
@@ -54,30 +54,19 @@ export const Top: React.FC<{ setAccessToken: (accessToken: string | null) => any
           className=""
           src={redBar}
         />
-        <div className="num-background">
-          <p className="num">1</p>
-          <p className="num-reserve-time">11:10</p>
-          <p className="num-elapse-time ">--分</p>
-          <p className="num-status">対応完了</p>
-        </div>
-        <div className="num-background">
-          <p className="num">2</p>
-          <p className="num-reserve-time">11:30</p>
-          <p className="num-elapse-time">--分</p>
-          <p className="num-status">呼び出し中</p>
-        </div>
-        <div className="num-background">
-          <p className="num">3</p>
-          <p className="num-reserve-time">12:30</p>
-          <p className="num-elapse-time">20分</p>
-          <p className="num-status">査定中</p>
-        </div>
-        <div className="num-background">
-          <p className="num">4</p>
-          <p className="num-reserve-time">12:40</p>
-          <p className="num-elapse-time">10分</p>
-          <p className="num-status">査定中</p>
-        </div>
+
+        
+         {data.map((data:SecondHand)=>(
+          <div className="num-background" key={data.id}>
+            <p className="num">{data.id}</p>
+            <p className="num-reserve-time">{data.reserve_time}</p>
+            <p className="num-elapse-time">{data.elapsed_time}</p>
+            <p className="num-status">{data.status}</p>
+            </div>
+         ))}
+          
+          
+        
       </div>
     );}
 
