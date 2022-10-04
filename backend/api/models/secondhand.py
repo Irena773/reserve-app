a848d7f07385d7c0bc1c2678bd97768274817ff1
Irena773 <ozawa.nanami.18@shizuoka.ac.jp>
2022-10-04 22:59:31 +0900

    受付時間と経過時間の型をdatetimeに変更

--- backend/api/models/secondhand.py
+++ backend/api/models/secondhand.py
@@ -1,12 +1,12 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, DATETIME
 from sqlalchemy.orm import relationship
-
+from datetime import datetime
 from api.db import Base
 
 class SecondHand(Base):
     __tablename__ = "secondhands"
 
     id = Column(Integer, primary_key=True)
-    reserve_time = Column(String(1024))
-    elapsed_time = Column(String(1024))
+    reserve_time = Column(DATETIME,default=datetime.now())
+    elapsed_time = Column(DATETIME,default=datetime.now())
     status = Column(String(1024))
--- backend/api/schemas/secondhand.py
+++ backend/api/schemas/secondhand.py
@@ -1,27 +1,27 @@
+from sqlite3 import Time
 from typing import Optional
 from pydantic import BaseModel, Field
 import datetime
 
-
 class SecondHandBase(BaseModel):
     status: Optional[str] = Field(None, example="査定中")
-    reserve_time: Optional[str] = Field(datetime.datetime.today())
-    elapsed_time: Optional[str] = Field(datetime.datetime.today())
+    reserve_time: Optional[datetime.datetime] = Field(datetime.datetime.now().isoformat())
+    elapsed_time: Optional[datetime.datetime] = Field(datetime.datetime.now().isoformat())
 
 class SecondHandRegister(SecondHandBase):
     pass
 
 class SecondHandRegisterResponse(SecondHandRegister):
     id:int
-    reserve_time: Optional[str] = Field(datetime.datetime.today())
-    elapsed_time: Optional[str] = Field(datetime.datetime.today())
+    reserve_time: Optional[datetime.datetime] = Field(datetime.datetime.now().isoformat())
+    elapsed_time: Optional[datetime.datetime] = Field(datetime.datetime.now().isoformat())
     class Config:
         orm_mode = True
 
 class SecondHand(SecondHandBase):
     id: int
-    reserve_time: Optional[str] = Field(datetime.datetime.today())
-    elapsed_time: Optional[str] = Field(datetime.datetime.today())
+    reserve_time: Optional[datetime.datetime] = Field(datetime.datetime.now().isoformat())
+    elapsed_time: Optional[datetime.datetime] = Field(datetime.datetime.now().isoformat())
     done: bool = Field(False, description="完了フラグ")
     class Config:
         orm_mode = True
